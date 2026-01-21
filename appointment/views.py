233f089848f6775304b602bddbe0ea85f6ab0d88
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-21 23:01:42 +0500

    update render_notes

--- appointment/views.py
+++ appointment/views.py
@@ -1,13 +1,24 @@
+from operator import ge
 from django.shortcuts import render
 
-from .selectors import get_appointments_by_client
+from .selectors import past_appointments_by_client, upcoming_appointments_by_client
+from beauty_city_web.client.selectors import get_client
 
 
 def render_notes(request):
-    appointments = get_appointments_by_client(client_id=request.GET.get("client_id"))
+    past_appointments = past_appointments_by_client(
+        client_id=request.GET.get("client_id")
+    )
+    upcoming_appointments = upcoming_appointments_by_client(
+        client_id=request.GET.get("client_id")
+    )
+    client = get_client(request.GET.get("client_id"))
     context = {
-        "appointments": appointments,
-        "has_appointments": appointments.exists(),
+        "past_appointments": past_appointments,
+        "upcoming_appointments": upcoming_appointments,
+        # "client": client,
+        "has_past_appointments": past_appointments.exists(),
+        "has_upcoming_appointments": upcoming_appointments.exists(),
     }
 
     return render(request, "notes.html", context=context)
