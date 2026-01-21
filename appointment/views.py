a1d44bc835b7cc9d7b9ba5f40458f3b5e9e43def
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-21 20:41:45 +0500

    update render_notes_page

--- appointment/views.py
+++ appointment/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from .selectors import get_appointments_by_client
 
 
-def notes_page(request, client_id):
-    appointments = get_appointments_by_client(client_id)
+def render_notes(request):
+    appointments = get_appointments_by_client(client_id=request.GET.get("client_id"))
     context = {
         "appointments": appointments,
         "has_appointments": appointments.exists(),
