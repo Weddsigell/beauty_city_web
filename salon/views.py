0153d75d322cebd081d1c8ecbb76531ab5a2f67c
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-24 20:24:31 +0500

    add login_required

--- salon/views.py
+++ salon/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import Salon
 
 
+@login_required
 def render_salons(request):
     context = {
         "salons": Salon.objects.all(),
