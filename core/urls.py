839f6a5a7c561aab47e78f5e72dbf3644aa6abb9
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-25 19:20:45 +0500

    connect admin

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.render_index, name="index_page"),
+    path("admin2/", views.render_admin, name="admin2_page"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils import timezone
 
 from master.models import Master
+from note.models import Note
 from procedure.models import Procedure
 from review.models import Review
 from salon.models import Salon
+from user.models import User
 
 
 def render_index(request):
@@ -28,3 +32,43 @@ def render_index(request):
         "reviews": Review.objects.all(),
     }
     return render(request, "index.html", context=context)
+
+
+def sum_mounth():
+    note_mounth = Note.objects.filter(
+        created_at__gte=(timezone.now() - timezone.timedelta(days=30)),
+        payment_status=True,
+    )
+    sum = 0
+    try:
+        for note in note_mounth:
+            sum += note.price
+
+        return sum
+    except Exception:
+        return sum
+
+
+@login_required
+def render_admin(request):
+    user_mounth = User.objects.filter(
+        created_at__gte=(timezone.now() - timezone.timedelta(days=30))
+    ).count()
+    note_mounth = Note.objects.filter(
+        created_at__gte=(timezone.now() - timezone.timedelta(days=30)),
+        payment_status=True,
+    ).count()
+    user_year = User.objects.filter(
+        created_at__gte=(timezone.now() - timezone.timedelta(days=365))
+    ).count()
+    percent = (note_mounth / user_mounth) * 100
+
+    context = {
+        "user": User.objects.get(id=request.user.id, is_superuser=True),
+        "user_mounth": user_mounth,
+        "user_year": user_year,
+        "percent_mounth": percent,
+        "sum_mounth": sum_mounth(),
+    }
+
+    return render(request, "admin.html", context=context)
